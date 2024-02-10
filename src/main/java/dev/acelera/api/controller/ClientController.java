e98d31455de87dc08e3b1331f49f5bfa2f2a2b70
Isailton89 <isailton2008@gmail.com>
2024-02-10 16:39:30 -0300

    Criado estrutura base do projeto e a classe ClientController

---
+++ src/main/java/dev/acelera/api/controller/ClientController.java
@@ -0,0 +1,4 @@
+package dev.acelera.api.controller;
+
+public class ClientController {
+}
---
+++ src/test/java/dev/acelera/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.acelera.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
