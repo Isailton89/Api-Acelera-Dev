4d6d2b8601b5a9a9a14691fc6102ba79da84fecd
Isailton89 <isailton2008@gmail.com>
2024-02-11 18:54:50 -0300

    Criado o metodo post do product com banco de dados e pasta que db.migration para gerar as tabelas

--- src/main/java/dev/acelera/api/controller/ProductController.java
+++ src/main/java/dev/acelera/api/controller/ProductController.java
@@ -1,16 +1,23 @@
 package dev.acelera.api.controller;
 
+import dev.acelera.api.product.Product;
 import dev.acelera.api.product.ProductRepository;
 import dev.acelera.api.product.ProductResgistrationData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("product")
 public class ProductController {
+
+    @Autowired
     private ProductRepository repository;
+
     @PostMapping
+    @Transactional
     public void register(@RequestBody ProductResgistrationData data) {
-        System.out.println(data);
+        repository.save(new Product(data));
     }
 
     @GetMapping
--- src/main/java/dev/acelera/api/product/Product.java
+++ src/main/java/dev/acelera/api/product/Product.java
@@ -1,11 +1,19 @@
 package dev.acelera.api.product;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Table(name = "products")
 @Entity(name = "Product")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Product {
 
     @Id
@@ -13,4 +21,9 @@ public class Product {
     private Long id;
     private String codigo;
     private BigDecimal preco;
+
+    public Product(ProductResgistrationData data) {
+        this.codigo = data.codigo();
+        this.preco = data.preco();
+    }
 }
